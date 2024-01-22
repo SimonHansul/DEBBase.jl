34028741d5f681d04b0dd885340f96d182315e44
SimonHansul <47112277+SimonHansul@users.noreply.github.com>
2024-01-22 13:59:55 +0100

    fixed error in calculation of damage (no damaga accumulation for embryos)

--- src/Simulators.jl
+++ src/Simulators.jl
@@ -9,25 +9,25 @@ function initialize_statevars(p::BaseParamCollection)::ComponentArray
         S = Float64(p.deb.X_emb_int * 0.01), # initial structure is a small fraction of initial reserve // mass of vitellus
         H = Float64(0.), # maturity
         R = Float64(0.), # reproduction buffer
-        life_stage = 1., # life stage 
-        I_emb = 0., # uptake from vitellus
-        I_p = 0., # uptake from external food resource
-        I = 0., # total uptake
-        A = 0., # assimilation
-        M = 0., # somatic maintenance
-        J = 0., # maturity maintenance 
-        C_W = p.glb.C_W, # external stressor concentrations
-        D_G = zeros(length(p.deb.k_D_G)), # scaled damage | growth efficiency
-        D_M = zeros(length(p.deb.k_D_M)), # scaled damage | maintenance costs 
-        D_A = zeros(length(p.deb.k_D_A)), # scaled damage | assimilation efficiency
-        D_R = zeros(length(p.deb.k_D_R)), # scaled damage | reproduction efficiency
-        D_h = zeros(length(p.deb.k_D_h)), # scaled damage | hazard rate
-        y_G = 1., # relative response | growth efficiency
-        y_M = 1., # relative response | maintenance costs 
-        y_A = 1., # relative response | assimilation efficiency
-        y_R = 1., # relative response | reproduction efficiency
-        h_z = 0., # hazard rate | chemical stressors
-        h_S = 0.  # hazard rate | starvation
+        life_stage = Float64(1.), # life stage 
+        I_emb = Float64(0.), # uptake from vitellus
+        I_p = Float64(0.), # uptake from external food resource
+        I = Float64(0.), # total uptake
+        A = Float64(0.), # assimilation
+        M = Float64(0.), # somatic maintenance
+        J = Float64(0.), # maturity maintenance 
+        C_W = (p.glb.C_W), # external stressor concentrations
+        D_G = MVector{length(p.deb.k_D_G), Float64}(zeros(length(p.deb.k_D_G))), # scaled damage | growth efficiency
+        D_M = MVector{length(p.deb.k_D_M), Float64}(zeros(length(p.deb.k_D_M))), # scaled damage | maintenance costs 
+        D_A = MVector{length(p.deb.k_D_A), Float64}(zeros(length(p.deb.k_D_A))), # scaled damage | assimilation efficiency
+        D_R = MVector{length(p.deb.k_D_R), Float64}(zeros(length(p.deb.k_D_R))), # scaled damage | reproduction efficiency
+        D_h = MVector{length(p.deb.k_D_h), Float64}(zeros(length(p.deb.k_D_h))), # scaled damage | hazard rate
+        y_G = Float64(1.), # relative response | growth efficiency
+        y_M = Float64(1.), # relative response | maintenance costs 
+        y_A = Float64(1.), # relative response | assimilation efficiency
+        y_R = Float64(1.), # relative response | reproduction efficiency
+        h_z = Float64(0.), # hazard rate | chemical stressors
+        h_S = Float64(0.)  # hazard rate | starvation
     )
 end
 
@@ -43,7 +43,8 @@ function simulator(
     assert!(p)
     u = initialize_statevars(p)
     prob = ODEProblem(DEB!, u, (0, p.glb.t_max), p) # define the problem
-    sol = solve(prob, Euler(), reltol = 1e-6, dt = 1/24, saveat = saveat) # get solution to the IVP
+    sol = solve(prob, Euler(), reltol = 1e-6, dt = 1/240) # get solution to the IVP
+    #sol = solve(prob, reltol = 1e-10, abstol = 1e-10, saveat = saveat) # get solution to the IVP
     simout = sol_to_df(sol) # convert solution to dataframe
   
     return simout
--- tests/basetest.jl
+++ tests/basetest.jl
@@ -1,35 +1,17 @@
 using Pkg; Pkg.activate("tests")
-using Plots, Plots.Measures, StatsPlots
-using BenchmarkTools
-default(leg = false, lw = 1.5)
-
 using Revise
-@time using DEBBase
-
+using DEBBase
+using Plots, StatsPlots
 
 p = BaseParamCollection()
-
-p.glb
-
-
-p.deb.k_D_G = [0.]
-p.deb.k_D_M = [0.]
-p.deb.k_D_A = [1.]
-p.deb.k_D_R = [0.]
-p.deb.drc_params_A = [(1., 2.)]
-
-p.glb.C_W = [0.]
-simout = simulator(p)
-plt = @df simout plot(
-    plot(:t, :S, ylabel = "S [μg C]"), 
-    plot(:t, :R ./ p.deb.X_emb_int, ylabel = "R [#]"), 
-    xlabel = "t", color = :black
-)
-
-Global
-
+isolate_pmoas!(p.deb, ["A"])
 p.glb.C_W = [0.5]
+p.deb.k_D_A = [1.]
+p.deb.drc_params_A = [(1., 2.)]
 simout = simulator(p)
-@df simout plot!(:t, :S, subplot = 1)
 
-@df simout plot(:t, :D_A_1, ylim = (0, Inf))
+@df simout plot(
+    plot(:t, :S),
+    plot(:t, :R),
+    plot(:t, :D_A_1)
+)
