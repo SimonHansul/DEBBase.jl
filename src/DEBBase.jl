659ba7ffe15374a6ffee29880817db5a7b6ef471
SimonHansul <simonhansul@gmail.com>
2024-02-05 12:32:20 +0100

    fixed Rdot

--- src/DEBBase.jl
+++ src/DEBBase.jl
@@ -10,6 +10,7 @@ using DocStringExtensions
 using DataFrames
 using PrecompileTools
 using StaticArrays
+using StatsBase
 
 include("Structures.jl")
 include("IO.jl")
@@ -28,6 +29,7 @@ BaseParamCollection,
 simulator,
 isolate_pmoas!,
 sig,
-clipneg
+clipneg,
+relative_response!
 
 end # module DEBBase
--- tests/test03_TD_singlestressors.jl
+++ tests/test03_TD_singlestressors.jl
@@ -1,64 +1,73 @@
-using Pkg; Pkg.activate("tests")
-using Revise
-@time using DEBBase
-@time using DoseResponse
-using Plots, StatsPlots, Plots.Measures
-default(leg = false, titlefontsize = 12, legendtitlefontsize = 10, lw = 1.5)
-using DataFrames
-
-using DEBBase
-
-deb = DEBBaseParams()
-isolate_pmoas!(deb, ["A"])
-deb.k_D_A = [1.]
-deb.drc_params_A = [(0.01, 2.)]
-glb = GlobalBaseParams()
-glb.C_W = [1.]
-θ = BaseParamCollection(glb = glb, deb = deb)
+begin
+    using Pkg; Pkg.activate("tests")
+    using Revise
+    @time using DEBBase
+    @time using DoseResponse
+    using Plots, StatsPlots, Plots.Measures
+    default(leg = false, titlefontsize = 12, legendtitlefontsize = 10, lw = 1.5)
+    using DataFrames, DataFramesMeta
+    using StatsBase
+    const TAG = replace(splitpath(@__FILE__)[end], ".jl" =>"") 
+end
 
 
-out = DataFrame()
-for pmoa in ["G", "M", "A", "R"]
-    for C_W in round.(10 .^ range(log10(0.1), log10(2.), length = 5), sigdigits = 2)
-        glb = GlobalBaseParams(t_max = 42., C_W = [C_W])
-        deb = DEBBaseParams(
-            k_D_G = [1.], 
-            k_D_M = [1.], 
-            k_D_A = [1.], 
-            k_D_R = [1.], 
-            k_D_h = [1.], 
-            drc_params_G = [(1., 2.)],
-            drc_params_M = [(1., 2.)],
-            drc_params_A = [(1., 2.)],
-            drc_params_R = [(1., 2.)],
-            drc_params_h = [(1., 2.)]
-            )
-        p = BaseParamCollection(glb = glb, deb = deb)
-        isolate_pmoas!(p.deb, [pmoa])
-        out_zj = simulator(p)
-        out_zj[!,:C_W] .= C_W
-        out_zj[!,:pmoa] .= pmoa
-        append!(out, out_zj)
+#=
+Simulate single stressors with different PMoAs
+=#
+begin   
+    out = DataFrame()
+    for pmoa in ["G", "M", "A", "R"]
+        for C_W in round.(10 .^ range(log10(0.1), log10(1.), length = 5), sigdigits = 2)
+            glb = GlobalBaseParams(t_max = 42., C_W = [C_W])
+            deb = DEBBaseParams(
+                k_D_G = [10.], 
+                k_D_M = [10.], 
+                k_D_A = [10.], 
+                k_D_R = [10.], 
+                k_D_h = [10.], 
+                drc_params_G = [(1., 2.)],
+                drc_params_M = [(1., 2.)],
+                drc_params_A = [(1., 2.)],
+                drc_params_R = [(1., 2.)],
+                drc_params_h = [(1., 2.)]
+                )
+            p = BaseParamCollection(glb = glb, deb = deb)
+            isolate_pmoas!(p.deb, [pmoa])
+            out_zj = simulator(p)
+            out_zj[!,:C_W] .= C_W
+            out_zj[!,:pmoa] .= pmoa
+            append!(out, out_zj)
+        end
+    end
+    out = DEBBase.relative_response(out, [:S, :R], :C_W; groupby_vars = [:t, :pmoa])
+
+
+    let pmoas = unique(out.pmoa), num_pmoas = length(pmoas)
+        plt = plot(
+            layout = (4, num_pmoas), 
+            title = hcat(vcat(pmoas, repeat([""], num_pmoas*3))...), 
+            size = (1000,800),
+            xlabel = hcat(vcat(
+                repeat([""], num_pmoas*3),
+                repeat(["t"], num_pmoas))...), 
+            ylabel = ["S" "" "" "" "y_S" "" "" "" "R" "" "" "" "y_R" "" "" ""],
+            bottommargin = 5mm, leftmargin = 5mm
+        )
+
+        for (j,pmoa) in enumerate(pmoas)
+            @df @subset(out, :pmoa .== pmoa) plot!(plt, :t, :S, group = :C_W, subplot = j, leg = j == 4 ? :bottomright : false, label = hcat(unique(:C_W)...), legendtitle = "C_W")
+            @df @subset(out, :pmoa .== pmoa) plot!(plt, :t, :y_S, group = :C_W, subplot = j+(1*num_pmoas))
+            @df @subset(out, :pmoa .== pmoa) plot!(plt, :t, :R, group = :C_W, subplot = j+(2*num_pmoas))
+            @df @subset(out, :pmoa .== pmoa) plot!(plt, :t, :y_R, group = :C_W, subplot = j+(3*num_pmoas))
+        end
+
+        display(plt)
+        savefig(plt, "plots/$(TAG).png")
     end
 end
 
-let pmoas = unique(out.pmoa), num_pmoas = length(pmoas)
-    plt = plot(
-        layout = (2, num_pmoas), 
-        title = hcat(vcat(pmoas, repeat([""], num_pmoas))...), 
-        size = (1000,450),
-        xlabel = hcat(vcat(
-            repeat([""], num_pmoas),
-            repeat(["t"], num_pmoas))...), 
-        ylabel = ["S" "" "" "" "R" "" "" ""],
-        bottommargin = 5mm, leftmargin = 5mm
-    )
 
-    [@df out[out.pmoa .== pmoa,:] plot!(plt, :t, :S, group = :C_W, subplot = j) for (j,pmoa) in enumerate(pmoas)]
-    [@df out[out.pmoa .== pmoa,:] plot!(plt, :t, :R, group = :C_W, subplot = j+num_pmoas) for (j,pmoa) in enumerate(pmoas)]
-
-    display(plt)
-end
+#=
 
 out = DataFrame()
 for pmoa in ["G", "M", "A", "R"]
@@ -684,3 +693,4 @@ let pmoas = unique(out.pmoa), num_pmoas = length(pmoas)
 
     display(plt)
 end
+=#
