69985fcb19a6627090b0d980f7c9ba7f54556106
SimonHansul <47112277+SimonHansul@users.noreply.github.com>
2024-03-18 22:14:40 +0100

    can define mutable struct in macro from vectors of fieldnames and defaults!!!

--- src/Macros.jl
+++ src/Macros.jl
@@ -5,6 +5,7 @@ if occursin("terminal", abspath(PROGRAM_FILE))
     using DEBParamStructs
     using Parameters
     using DataFrames, DataFramesMeta
+    @time using DEBBase
 end
 
 include("Structures.jl")
@@ -25,17 +26,69 @@ end
 
 
 """
-Macro to generate
+Macro to generate parameter structure
 """
 macro defparams(Name, field, val)
     eval(genstruct(Name, field, val))
 end
 
+@defparams SomeNewParams gamma 0.5
+SomeNewParams().gamma
 
-@defparams PASF gamma 0.5
-@defparams wholenotherparams blabla [x -> 1 + x, x -> 3x]
+#=
+Ok, we know now how to define a mutable struct inlcuding defaults within a macro. 
+The next step is to extend this to multiple fields.
+=#
 
-PASF().gamma
+function genstruct(Name, fields, vals)
+    exprs = Expr[]
+    for (field, val) in zip(fields, vals)
+        push!(exprs, :( $(Symbol(field)) = $val))
+    end
+
+    quote 
+        @with_kw mutable struct $Name
+            $(exprs...)
+        end
+    end
+end
+
+"""
+Macro to generate parameter structure
+"""
+macro defparams(Name, fields, vals)
+    eval(genstruct(Name, fields, vals))
+end
+
+expr = genstruct(:Bloahr, [:k, :g], [1., 3.])
+
+obj = eval(expr)
+
+obj()
+
+
+(exprs...)
+
+
+@defparams MultipleParams [:kappa, :gamma] [0.5, 0.8]
+
+
+@with_kw mutable struct Ehm
+    x = 1; y = 2
+end
+
+
+Ehm()
+
+fields = [:p1, :p2]
+vals = [0.5, 1.0]
+
+e = Expr(:tuple, [:( $field = $val ) for (field, val) in zip(fields, vals)]...)
+
+eval(e)
+
+
+
 
 
 wholenotherparams()
